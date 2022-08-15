682cef7f1e6c099b03dabea59331b455df197cff
atyoshimatsu <at.yoshimatsu@gmail.com>
2022-08-15 10:26:52 -0700

    Implement backward search with Janet

--- wordsearch.js
+++ wordsearch.js
@@ -1,6 +1,7 @@
 const wordSearch = (letters, word) => {
   const horizontalJoin = letters.map(ls => ls.join(''));
   const verticalJoin = transpose(letters).map(ls => ls.join(''));
+  const reversedHorizontalJoin = horizontalJoin.map(s => s.split('').reverse().join(''));
 
   for (const l of horizontalJoin) {
     if (l.includes(word)) return true;
@@ -9,6 +10,12 @@ const wordSearch = (letters, word) => {
   for (const l of verticalJoin) {
     if (l.includes(word)) return true;
   }
+
+  for (const l of reversedHorizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+
+
   return false;
 };
 
