aecae457bb5f36fa81c5ee4ec4f0bc854aebb56b
atyoshimatsu <at.yoshimatsu@gmail.com>
2022-08-15 10:18:35 -0700

    Imlplement vertically search paired with Janet

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,30 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  const verticalJoin = transpose(letters).map(ls => ls.join(''));
 
-module.exports = wordSearch
+  for (const l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  for (const l of verticalJoin) {
+    if (l.includes(word)) return true;
+  }
+  return false;
+};
+
+module.exports = wordSearch;
+
+
+const transpose = function(matrix) {
+  const transposed = [];
+  // c: column
+  for (let c = 0; c < matrix[0].length; c++) {
+    transposed.push([]);
+    // r: row
+    for (let r = 0; r < matrix.length; r++) {
+      transposed[c].push(matrix[r][c]);
+    }
+  }
+
+  return transposed;
+};
